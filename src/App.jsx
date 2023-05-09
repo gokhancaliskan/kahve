ccdd5c9c4e7d8aefe7e857b0e6ee587d00765a3d
Gökhan Çalışkan <agooqle@gmail.com>
2023-05-10 00:29:00 +0300

    bugfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar.jsx";
 import Products from "./components/Products.jsx";
-import Footer from "./components/Footer.jsx";
+
 import "./styles.css";
 
 const App = () => {
@@ -8,7 +8,6 @@ const App = () => {
 		<div>
 			<Navbar></Navbar>
 			<Products></Products>
-			<Footer></Footer>
 		</div>
 	);
 };
--- src/components/Footer.jsx
+++
@@ -1,5 +0,0 @@
-const Footer = () => {
-	return <div>GOKHAN CALISKAN</div>;
-};
-
-export default Footer;
